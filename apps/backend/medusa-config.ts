7420fe78d74fecaaa8121c772f9b59ea7e4df5f6
luxumbra <me@luxumbra.dev>
2025-07-21 18:02:45 +0100

    some updates i want to merege with develop

--- apps/backend/medusa-config.ts
+++ apps/backend/medusa-config.ts
@@ -39,5 +39,27 @@ module.exports = defineConfig({
         ],
       },
     },
+    {
+      resolve: "@medusajs/medusa/payment",
+      options: {
+        providers: [
+          {
+            resolve: "@medusajs/medusa/payment-stripe",
+            id: "stripe",
+            options: {
+              apiKey: process.env.STRIPE_SECRET_KEY,
+              webhookSecret: process.env.STRIPE_WEBHOOK_SECRET,
+              capture: true,
+            },
+          },
+        ],
+      },
+    },
+    // {
+    //   resolve: "./src/modules/wishlist",
+    //   options: {
+    //     jwtSecret: process.env.JWT_SECRET || 'supersecret'
+    //   }
+    // }
   ],
 });
