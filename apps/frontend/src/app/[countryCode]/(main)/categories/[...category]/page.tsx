5f6bf6a43507d7634224f6c699cbb445e5104483
luxumbra <me@luxumbra.dev>
2025-08-04 04:04:22 +0100

    fix hard coded localhost backend url

--- apps/frontend/src/app/[countryCode]/(main)/categories/[...category]/page.tsx
+++ apps/frontend/src/app/[countryCode]/(main)/categories/[...category]/page.tsx
@@ -1,11 +1,11 @@
-import { Metadata } from "next"
+import type { Metadata } from "next"
 import { notFound } from "next/navigation"
 
 import { getCategoryByHandle, listCategories } from "@lib/data/categories"
 import { listRegions } from "@lib/data/regions"
-import { StoreRegion } from "@medusajs/types"
+import type { StoreRegion } from "@medusajs/types"
 import CategoryTemplate from "@modules/categories/templates"
-import { SortOptions } from "@modules/store/components/refinement-list/sort-products"
+import type { SortOptions } from "@modules/store/components/refinement-list/sort-products"
 
 type Props = {
   params: Promise<{ category: string[]; countryCode: string }>
